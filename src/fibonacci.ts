23c42d4e4f8ca56e0761565b0d5c1523f9ddeebf
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:54:23 +0100

    compute fibonnaci numbers in an array

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -18,6 +18,10 @@ export const computeFibonacciNumber = (position: number | null): number => {
     let i = 1;
     let j = 1;
 
+    if (position <= 2) {
+        return 1;
+    }
+
     let currentPosition = 2;
     while (currentPosition < notNullPosition) {
         const temp = i;
@@ -27,6 +31,10 @@ export const computeFibonacciNumber = (position: number | null): number => {
     }
     return j;
 };
+export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
+    const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
+    return inputArray.map(x => computeFibonacciNumber(x));
+}
 
 const computeNegativeFibonacci = (position: number): number => {
     if (position >= 0) {
